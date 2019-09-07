dbe81ff3616afd19c5f07ecbcf6bc73e7f6e0011
Dean Gilewicz <gilewiczd@gmail.com>
2019-09-07 01:05:55 -0600

    updated validation logic for interests

--- models/interest.js
+++ models/interest.js
@@ -8,7 +8,18 @@ module.exports = (sequelize, type) => {
 		interest: {
 			type: type.STRING,
 			validate: {
-				is: ["^[a-zA-Z0-9_]*$"]
+				validateInterest: function(value) {
+					const reFind = new RegExp("[-, ,_]", 'g');
+					const reIsAlphaNumeric = new RegExp("^[a-z0-9]+$", 'i');
+					// remove spaces, hyphens, and underscores
+					const sanitizedValue = value.replace(reFind, '');
+					// check if alphanumeric
+					if (!reIsAlphaNumeric.test(sanitizedValue)) {
+						throw new Error(
+							"Interests can only contain alphanumeric, hyphen or underscore characters"
+						);
+					}
+				}
 			}
 		}
 	});
