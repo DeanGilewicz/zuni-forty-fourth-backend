da3ba11d7050ae4ee6b2c90072cd6d04df464568
Dean Gilewicz <gilewiczd@gmail.com>
2020-03-23 21:02:53 -0600

    refactor to include ssh tunnel as needed for testing

---
+++ seedData.js
@@ -0,0 +1,325 @@
+module.exports.properties = [
+	{
+		streetAddress: "123 Demo Ave",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	},
+	{
+		streetAddress: "456 Demo Ave",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	},
+	{
+		streetAddress: "789 Demo Ave",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	},
+	{
+		streetAddress: "1011 Demo Ave",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	},
+	{
+		streetAddress: "1213 Demo Ave",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	},
+	{
+		streetAddress: "1415 Demo Street",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	},
+	{
+		streetAddress: "1617 Demo Street",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	},
+	{
+		streetAddress: "1819 Demo Street",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	},
+	{
+		streetAddress: "2021 Demo Street",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	},
+	{
+		streetAddress: "2223 Demo Street",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	},
+	{
+		streetAddress: "2425 Demo Street",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	}
+];
+
+module.exports.users = [
+	{
+		verifyCode: "",
+		verifyCodeExpiration: 1111111111,
+		firstName: "admin",
+		lastName: "user",
+		emailAddress: "admin@test.com",
+		password:
+			"$2b$10$KooGtWwb6s/Dqe.gBjpyN.9Bt5lgNdTc88TH2d0.cEH2NGCQjc.m2",
+		phoneNumber: "",
+		image:
+			"https://res.cloudinary.com/cloudassets/image/upload/q_auto,f_auto/v1565501442/zuni44/profile-placeholder.png"
+	},
+	{
+		verifyCode: "",
+		verifyCodeExpiration: 1212121212,
+		firstName: "michael",
+		lastName: "scott",
+		emailAddress: "ms@test.com",
+		password:
+			"$2b$10$KooGtWwb6s/Dqe.gBjpyN.9Bt5lgNdTc88TH2d0.cEH2NGCQjc.m2",
+		phoneNumber: "1020121021",
+		image:
+			"https://res.cloudinary.com/cloudassets/image/upload/q_auto,f_auto/v1565501442/zuni44/profile-placeholder.png"
+	},
+	{
+		verifyCode: "",
+		verifyCodeExpiration: 2222222222,
+		firstName: "pam",
+		lastName: "beasley",
+		emailAddress: "pb@test.com",
+		password:
+			"$2b$10$KooGtWwb6s/Dqe.gBjpyN.9Bt5lgNdTc88TH2d0.cEH2NGCQjc.m2",
+		phoneNumber: "9992223333",
+		image:
+			"https://res.cloudinary.com/cloudassets/image/upload/q_auto,f_auto/v1565501442/zuni44/profile-placeholder.png"
+	},
+	{
+		verifyCode: "",
+		verifyCodeExpiration: 3333333333,
+		firstName: "jim",
+		lastName: "helburt",
+		emailAddress: "jh@test.com",
+		password:
+			"$2b$10$KooGtWwb6s/Dqe.gBjpyN.9Bt5lgNdTc88TH2d0.cEH2NGCQjc.m2",
+		phoneNumber: "2244668899",
+		image:
+			"https://res.cloudinary.com/cloudassets/image/upload/q_auto,f_auto/v1565501442/zuni44/profile-placeholder.png"
+	},
+	{
+		verifyCode: "",
+		verifyCodeExpiration: 4444444444,
+		firstName: "kevin",
+		lastName: "malone",
+		emailAddress: "km@test.com",
+		password:
+			"$2b$10$KooGtWwb6s/Dqe.gBjpyN.9Bt5lgNdTc88TH2d0.cEH2NGCQjc.m2",
+		phoneNumber: "8787878787",
+		image:
+			"https://res.cloudinary.com/cloudassets/image/upload/q_auto,f_auto/v1565501442/zuni44/profile-placeholder.png"
+	},
+	{
+		verifyCode: "",
+		verifyCodeExpiration: 5555555555,
+		firstName: "holly",
+		lastName: "flax",
+		emailAddress: "hf@test.com",
+		password:
+			"$2b$10$KooGtWwb6s/Dqe.gBjpyN.9Bt5lgNdTc88TH2d0.cEH2NGCQjc.m2",
+		phoneNumber: "7878787878",
+		image:
+			"https://res.cloudinary.com/cloudassets/image/upload/q_auto,f_auto/v1565501442/zuni44/profile-placeholder.png"
+	},
+	{
+		verifyCode: "",
+		verifyCodeExpiration: 6666666666,
+		firstName: "andy",
+		lastName: "bernart",
+		emailAddress: "ab@test.com",
+		password:
+			"$2b$10$KooGtWwb6s/Dqe.gBjpyN.9Bt5lgNdTc88TH2d0.cEH2NGCQjc.m2",
+		phoneNumber: "6767676767",
+		image:
+			"https://res.cloudinary.com/cloudassets/image/upload/q_auto,f_auto/v1565501442/zuni44/profile-placeholder.png"
+	},
+	{
+		verifyCode: "",
+		verifyCodeExpiration: 7777777777,
+		firstName: "phillis",
+		lastName: "vance",
+		emailAddress: "pv@test.com",
+		password:
+			"$2b$10$KooGtWwb6s/Dqe.gBjpyN.9Bt5lgNdTc88TH2d0.cEH2NGCQjc.m2",
+		phoneNumber: "3434343434",
+		image:
+			"https://res.cloudinary.com/cloudassets/image/upload/q_auto,f_auto/v1565501442/zuni44/profile-placeholder.png"
+	},
+	{
+		verifyCode: "",
+		verifyCodeExpiration: 8888888888,
+		firstName: "dwight",
+		lastName: "schrute",
+		emailAddress: "ds@test.com",
+		password:
+			"$2b$10$KooGtWwb6s/Dqe.gBjpyN.9Bt5lgNdTc88TH2d0.cEH2NGCQjc.m2",
+		phoneNumber: "5656565656",
+		image:
+			"https://res.cloudinary.com/cloudassets/image/upload/q_auto,f_auto/v1565501442/zuni44/profile-placeholder.png"
+	},
+	{
+		verifyCode: "",
+		verifyCodeExpiration: 9999999999,
+		firstName: "oscar",
+		lastName: "martinex",
+		emailAddress: "om@test.com",
+		password:
+			"$2b$10$KooGtWwb6s/Dqe.gBjpyN.9Bt5lgNdTc88TH2d0.cEH2NGCQjc.m2",
+		phoneNumber: "4545454545",
+		image:
+			"https://res.cloudinary.com/cloudassets/image/upload/q_auto,f_auto/v1565501442/zuni44/profile-placeholder.png"
+	}
+];
+
+module.exports.userStatus = [
+	{
+		status: "1",
+		name: "Requested"
+	},
+	{
+		status: "2",
+		name: "Approved"
+	},
+	{
+		status: "3",
+		name: "Password"
+	}
+];
+
+module.exports.userRoles = [
+	{
+		role: "1",
+		name: "Admin"
+	},
+	{
+		role: "2",
+		name: "Owner"
+	},
+	{
+		role: "3",
+		name: "User"
+	}
+];
+
+module.exports.userInterests = [
+	{
+		userId: 2,
+		interest: "Coding"
+	},
+	{
+		userId: 2,
+		interest: "Golf"
+	},
+	{
+		userId: 2,
+		interest: "Soccer"
+	},
+	{
+		userId: 3,
+		interest: "Travel"
+	},
+	{
+		userId: 3,
+		interest: "Eating"
+	},
+	{
+		userId: 4,
+		interest: "Playing"
+	},
+	{
+		userId: 3,
+		interest: "Sleeping"
+	},
+	{
+		userId: 3,
+		interest: "Eating"
+	}
+];
+
+module.exports.events = [
+	{
+		cost: 0,
+		start: "2019-05-22 15:30:00",
+		end: "2019-05-23 17:30:00",
+		name: "Restaurant",
+		address: "123 Cool Street",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	},
+	{
+		cost: 10,
+		start: "2019-05-19 14:00:00",
+		end: "2019-05-19 16:00:00",
+		name: "Conference",
+		address: "343 West Lake",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	},
+	{
+		cost: 40,
+		start: "2019-06-10 10:45:00",
+		end: "2019-06-10 11:45:00",
+		name: "Coffee Shop",
+		address: "822 Nice Lane",
+		city: "Scranton",
+		state: "PA",
+		zipCode: 18447
+	}
+];
+
+module.exports.companies = [
+	{
+		type: "electrical",
+		name: "Xcel Energy",
+		email: "support@xcel.com",
+		contactNumber: "3032123321",
+		website: "https://www.xcelenergy.com",
+		address: "222 Energy Lane",
+		city: "Scranton",
+		state: "PA",
+		zipCode: "18447"
+	},
+	{
+		type: "builder",
+		name: "Strong Houses",
+		email: "",
+		contactNumber: "",
+		website: "",
+		address: "812 Builder Lane",
+		city: "Scranton",
+		state: "PA",
+		zipCode: "18447"
+	},
+	{
+		type: "internet-tv",
+		name: "Century Link",
+		email: "",
+		contactNumber: "",
+		website: "",
+		address: "812 Century Road",
+		city: "Scranton",
+		state: "PA",
+		zipCode: "18447"
+	}
+];
